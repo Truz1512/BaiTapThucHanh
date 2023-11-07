c8d5c438a5505a2b5601c9e419efebef83062c39
Truz1512 <truongmai564@gmail.com>
2023-11-07 17:44:20 +0700

    Slide 34

--- PHP/index.php
+++ PHP/index.php
@@ -1,11 +1,17 @@
-
-<?php
-    $a = 5;
-    $b = 7;
-    if ($a == $b) {
-        echo "Day la hinh vuong";
-    }
-    else {
-        echo "Day la hinh chu nhat";
-    }
-?>
+<!DOCTYPE html>
+<html lang="en" dir= "ltr">
+    <head>
+        <meta charset= "uft-8">
+        <title></title>
+    </head>
+    <body>
+        <form action= "5-formGet.php" method="get">
+            <p>Name: </p>
+            <input type="text" name = "name"><br>
+            <p>E-mail: </p>
+            <input type="text" name = "email"><br>
+            <p></p>
+            <input type="submit">
+        </form>
+    </body>
+</html>
