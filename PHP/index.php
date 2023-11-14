fc9c093e6bd82910583433e7fcc39d7dc391e4e7
Truz1512 <truongmai564@gmail.com>
2023-11-14 15:04:03 +0700

    Slide 3

--- PHP/index.php
+++ PHP/index.php
@@ -1,25 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" dir= "ltr">
-    <head>
-        <meta charset= "utf-8">
-        <title></title>
-    </head>
-    <body>
-        <!-- nhap thong tin -->
-        <form action= "result.php" method="post">
-            <p>Name: </p>
-            <input type="text" name = "name"><br>
-            <p>Password: </p>
-            <input type="password" name = "password"><br>
-            <p>SDT: </p>
-            <input type="number" name = "sodienthoai"><br>
-            <p>Email: </p>
-            <input type="text" name = "email"><br>
-            <p>Dia chi: </p>
-            <input type="text" name = "diachi"><br>
-            <p></p>
-            <button type="submit">Dang ki</button>
-        </form>
-        
-    </body>   
-</html>
+<?php
+    $items = array('item1.png', 'item2.png', 'item3.png', 'item4.png');
+    foreach ($items as $item)
+    {
+        echo "<img src='image/$item' width = 50px height = 50px alt='$item'>";
+    }
+?>
