2b1d31613f3d16f454038d75bfb4bbe36d1ff47c
Truz1512 <truongmai564@gmail.com>
2023-11-14 15:06:33 +0700

    Slide 7

--- PHP/index.php
+++ PHP/index.php
@@ -1,7 +1,6 @@
 <?php
-    $items = array('item1.png', 'item2.png', 'item3.png', 'item4.png');
-    foreach ($items as $item)
+    for ($x = 1; $x <= 10; $x +=1)
     {
-        echo "<img src='image/$item' width = 50px height = 50px alt='$item'>";
+        echo "Day la so $x <br>";
     }
 ?>
