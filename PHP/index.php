b45f0f87c781ac2a37ae31381a956e489884b08e
Truz1512 <truongmai564@gmail.com>
2023-11-14 15:25:58 +0700

    Booleans slide2

--- PHP/index.php
+++ PHP/index.php
@@ -1,9 +1,24 @@
-<?php
-    $x = 1;
-    while ($x <= 10)
-    {
-        echo "Day la so: $x <br>";
-        $x++;
-    }
-    
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $T = True;
+            $F = False;
+            echo "T = $T <br>";
+            echo "F = $F <br>";
+
+            $check1 = 10 > 5;
+            $check2 = 20 < 10;
+            $check3 = $check1 && $check2;
+            $check4 = $check1 || $check2;
+            echo "check1 = $check1 <br>";
+            echo "check2 = $check2 <br>";
+            echo "check3 = $check3 <br>";
+            echo "check4 = $check4";
+        ?>
+    </body>
+</html>
