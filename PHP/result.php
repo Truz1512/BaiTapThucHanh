a85b2bd4ef9bf9d18709fcda8b507aafe1f2353d
Truz1512 <truongmai564@gmail.com>
2023-11-17 10:26:40 +0700

    Slide 32

--- PHP/result.php
+++ PHP/result.php
@@ -6,17 +6,26 @@
     </head>
     <body>
         <?php
-            $exchangeRate = array(22300, 27300, 17000, 120);
-            $exchangeRate[] = 600;
-            $exchangeRate[] = 16000;
-            $exchangeRate[0] = 22200;
-            echo "<h3>Exchange rate to VND</h3>";
-            echo "USD: $exchangeRate[0]<br>";
-            echo "EUR: $exchangeRate[1]<br>";
-            echo "SGD: $exchangeRate[2]<br>";
-            echo "JPY: $exchangeRate[3]<br>";
-            echo "CHN: $exchangeRate[4]<br>";
-            echo "AUD: $exchangeRate[5]<br>";
+            $USD = 22300;
+            $EUR = 27300;
+            $SGD = 17000;
+            $JPY = 120;
+            $amount = $_GET['amount'];
+            echo "$amount USD is equal ";
+            if ($_GET["currency"] == "USD")
+            {
+                echo $amount + $USD;
+            }
+            elseif ($_GET["currency"] == "EUR") {
+                echo $amount + $EUR;
+            }
+            elseif ($_GET["currency"] == "SGD") {
+                echo $amount + $SGD;
+            }
+            else {
+                echo $amount + $JPY;
+            }
+            echo "VND"
         ?>
     </body>
 </html>
