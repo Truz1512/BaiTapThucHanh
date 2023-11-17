9be2f3d7db4138a52b8d0c005d63b93b93648ae2
Truz1512 <truongmai564@gmail.com>
2023-11-17 10:09:59 +0700

    Slide 24

--- PHP/index.php
+++ PHP/index.php
@@ -2,21 +2,25 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Kiểm tra yêu cầu công ty</title>
+        <title></title>
     </head>
     <body>
-        <h1>Kiểm tra yêu cầu công ty</h1>
-        <form action="result.php" method="post">
-            <label for="chieucao">Chiều cao: </label>
-            <input type="number" name="chieucao"><br>
-            <label for="cannang">Cân nặng: </label>
-            <input type="number" name="cannang"><br>
-            <label for="gioitinh">Giới tính </label>
-            <select name="gioitinh">
-                <option name="nam">Nam</option>
-                <option name="nu">Nữ</option>
-            </select><br>
-            <input type="submit" value="Kiểm tra">
-        </form>
+        <?php
+            $currency = $_GET['currency'];
+            echo "$amount $currency is equal ";
+            switch (%$currency) {
+                case "USD":
+                    echo $amount * $USD;
+                    break;
+                case "EUR":
+                    echo $amount * $EUR;
+                    break;
+                case "SGD":
+                    echo $amount * $SGD;
+                    break;
+                case "JPY":
+                    echo $amount * $JPY;
+                    break;
+            }
     </body>
 </html>
