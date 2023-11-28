ca71d70cafead8f86a10b6500ff6f98de2aa7198
Truz1512 <truongmai564@gmail.com>
2023-11-28 17:28:01 +0700

    PHP Slide 50 Fix

--- PHP/index.php
+++ PHP/index.php
@@ -5,35 +5,35 @@
     <title></title>
 </head>
 <body>
-    <formm action="" method="get">
+    <form action="" method="get">
         <p>First number: </p>
         <input type="number" name="firstNumber"><br>
         <p>Second number: </p>
         <input type="number" name="secondNumber"><br>
         <p></p>
-        <input type="submit name="add" value="+">
+        <input type="submit" name="add" value="+">
         <input type="submit" name="minus" value="-">
         <input type="submit" name="multiply" value="*">
         <input type="submit" name="devide" value="/">
-    </formm>
+    </form>
 
 <?php
     if (isset($_GET['add'])){
-        $result = $_GET["firstNumbrt"] + $GET["secondNumber"];
+        $result = $_GET["firstNumber"] + $_GET["secondNumber"];
         echo $result;
     }
     elseif (isset($_GET['minus'])){
-        $result = $_GET["firstNumbrt"] - $_GET["secondNumber"];
+        $result = $_GET["firstNumber"] - $_GET["secondNumber"];
         echo $result;
     }
     elseif (isset($_GET['multiply'])){
-        $result = $_GET["firstNumbrt"] * $_GET["secondNumber"];
+        $result = $_GET["firstNumber"] * $_GET["secondNumber"];
         echo $result;
     }
-    elseif (isset($_GET['devide')){
-        $result = $_GET["firstNumbrt"] / $_GET["secondNumber"];
-        echo $result
+    elseif (isset($_GET['devide'])){
+        $result = $_GET["firstNumber"] / $_GET["secondNumber"];
+        echo $result;
     }
-
+?>
 </body>
 </html>
