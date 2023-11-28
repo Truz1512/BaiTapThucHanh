e1eddb388d406ac905146a10c461087462864d7f
Truz1512 <truongmai564@gmail.com>
2023-11-28 16:57:22 +0700

    PHP Slide 26

--- PHP/index.php
+++ PHP/index.php
@@ -2,31 +2,28 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculator</title>
+    <title></title>
 </head>
 <body>
-    <h1>Calculator</h1>
-    <form action="result.php" method="post">
-        <label for="toan">Toán:</label>
-        <input type="number" name="toan" step="0.1" min="0" max="10" required><br>
-
-        <label for="ly">Lý:</label>
-        <input type="number" name="ly" step="0.1" min="0" max="10" required><br>
-
-        <label for="hoa">Hóa:</label>
-        <input type="number" name="hoa" step="0.1" min="0" max="10" required><br>
-
-        <label for="tienganh">Tiếng Anh:</label>
-        <input type="number" name="tienganh" step="0.1" min="0" max="10" required><br>
-
-        <label for="van">Văn:</label>
-        <input type="number" name="van" step="0.1" min="0" max="10" required><br>
-
-        <label for="lichsu">Lịch Sử:</label>
-        <input type="number" name="lichsu" step="0.1" min="0" max="10" required><br>
-
-        <button type="submit">Tính điểm trung bình</button>
+    <h3>Currency exchange</h3>
+    <form action="" method="get">
+        <p>Amount:</p>
+        <input type="number" name="amount">
+        <p>Select currency</p>
+        <select name="currency">
+            <option value="USD">USD</option>
+            <option value="EUR">EURO</option>
+            <option value="SGD">SINGAPORE DOLAR</option>
+            <option value="JPY">JAPANESE YEN</option>
+        </select>
+        <input type="submit" value="Convert">
     </form>
 </body>
+<?php
+    $exchangeRate = array("USD"=>22300, "EUR"=>27300, "SGD"=>17000, "JPY"=>120);
+    if (isset($_GET['amount'])){
+        $amount = $_GET['amount'];
+        $currency = $_GET["currency"];
+        echo " $amount USD is equal " . number_format($amount * $exchangeRate[$currency]) . "VND";
+    }
 </html>
