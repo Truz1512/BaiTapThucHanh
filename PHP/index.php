889559cd016ba105c4db3a76f13954623f4df32f
Truz1512 <truongmai564@gmail.com>
2023-11-28 16:03:52 +0700

    PHP Slide 23

--- PHP/index.php
+++ PHP/index.php
@@ -2,7 +2,7 @@
     function display_image_list($imageList)
     {
         foreach ($imageList as $item) {
-            echo "img src='images/$item'
+            echo "img src='image/$item'
             width = 50px height = 50px alt='$item'>";
         }
     }
@@ -10,8 +10,7 @@
 ?>
 <h1>function</h1>
 <?php
-    $item = array('item5.png', 'item6.png');
-    foreach ($item as $item) {
-        echo "<img src='images/$item' width = 50px height = 50px alt='$item'>";
-    }
+    require('13-function.php');
+    $item = array('item1.png', 'item2.png', 'item3.png', 'item4.png');
+    display_image_list($item);
 ?>
