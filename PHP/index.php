ab9ad3c75f0d2726d08fabe64d1b5aab44bca03f
Truz1512 <truongmai564@gmail.com>
2023-11-28 16:18:57 +0700

    PHP Slide 25

--- PHP/index.php
+++ PHP/index.php
@@ -1,16 +1,32 @@
-<?php
-    function display_image_list($imageList)
-    {
-        foreach ($imageList as $item) {
-            echo "img src='image/$item'
-            width = 50px height = 50px alt='$item'>";
-        }
-    }
-    
-?>
-<h1>function</h1>
-<?php
-    require('13-function.php');
-    $item = array('item1.png', 'item2.png', 'item3.png', 'item4.png');
-    display_image_list($item);
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+</head>
+<body>
+    <h1>Calculator</h1>
+    <form action="result.php" method="post">
+        <label for="toan">Toán:</label>
+        <input type="number" name="toan" step="0.1" min="0" max="10" required><br>
+
+        <label for="ly">Lý:</label>
+        <input type="number" name="ly" step="0.1" min="0" max="10" required><br>
+
+        <label for="hoa">Hóa:</label>
+        <input type="number" name="hoa" step="0.1" min="0" max="10" required><br>
+
+        <label for="tienganh">Tiếng Anh:</label>
+        <input type="number" name="tienganh" step="0.1" min="0" max="10" required><br>
+
+        <label for="van">Văn:</label>
+        <input type="number" name="van" step="0.1" min="0" max="10" required><br>
+
+        <label for="lichsu">Lịch Sử:</label>
+        <input type="number" name="lichsu" step="0.1" min="0" max="10" required><br>
+
+        <button type="submit">Tính điểm trung bình</button>
+    </form>
+</body>
+</html>
--- PHP/result.php
+++ PHP/result.php
@@ -1,15 +1,44 @@
-<!DOCTYPE html>
-<html lang="en" dir= "ltr">
-    <head>
-        <meta charset= "utf-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                $select_year = $_POST['year'];
-                echo "Ban da chon nam: $select_year";
-            }
-        ?>
-    </body>
-</html>
+<?php
+
+function tinhDiemTrungBinh($diemToan, $diemLy, $diemHoa, $diemTiengAnh, $diemVan, $diemLichSu) {
+    $diemTong = $diemToan + $diemLy + $diemHoa + $diemTiengAnh + $diemVan + $diemLichSu;
+    $diemTrungBinh = $diemTong / 6;
+
+    return $diemTrungBinh;
+}
+
+function xepLoai($diemTrungBinh) {
+    if ($diemTrungBinh < 5) {
+        return "Học sinh yếu";
+    } elseif ($diemTrungBinh >= 5 && $diemTrungBinh <= 6.4) {
+        return "Học sinh trung bình";
+    } elseif ($diemTrungBinh >= 6.5 && $diemTrungBinh <= 7.9) {
+        return "Học sinh khá";
+    } else {
+        return "Học sinh giỏi";
+    }
+}
+
+// Nhận dữ liệu từ biểu mẫu
+$diemToan = isset($_POST['toan']) ? floatval($_POST['toan']) : 0;
+$diemLy = isset($_POST['ly']) ? floatval($_POST['ly']) : 0;
+$diemHoa = isset($_POST['hoa']) ? floatval($_POST['hoa']) : 0;
+$diemTiengAnh = isset($_POST['tienganh']) ? floatval($_POST['tienganh']) : 0;
+$diemVan = isset($_POST['van']) ? floatval($_POST['van']) : 0;
+$diemLichSu = isset($_POST['lichsu']) ? floatval($_POST['lichsu']) : 0;
+
+// Kiểm tra và tính điểm trung bình
+if ($diemToan >= 0 && $diemToan <= 10 &&
+    $diemLy >= 0 && $diemLy <= 10 &&
+    $diemHoa >= 0 && $diemHoa <= 10 &&
+    $diemTiengAnh >= 0 && $diemTiengAnh <= 10 &&
+    $diemVan >= 0 && $diemVan <= 10 &&
+    $diemLichSu >= 0 && $diemLichSu <= 10) {
+
+    $diemTrungBinh = tinhDiemTrungBinh($diemToan, $diemLy, $diemHoa, $diemTiengAnh, $diemVan, $diemLichSu);
+    $xepLoai = xepLoai($diemTrungBinh);
+
+    echo "Điểm trung bình: $diemTrungBinh<br>";
+    echo "Xếp loại: $xepLoai";
+} else {
+}
